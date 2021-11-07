2dde715d1c492be8a408d8805c271bc2abed6446
Zulker Nine Rokey <85927902+ZulkerNineRokey@users.noreply.github.com>
2021-11-07 17:04:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+$('#one-popup').magnificPopup({
+    items: [
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#two-popup').magnificPopup({
+    items: [
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('#three-popup').magnificPopup({
+    items: [
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: '/clip2.png',
+        title: 'Peter & Paul fortress in SPB'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
